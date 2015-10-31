5ab4f5136235ac3c19350397319dfd8bc934021a
Alonso Holmes <hello@alonso.io>
2015-10-31 17:44:57 +0800

    good starting point

---
+++ app/app.js
@@ -0,0 +1,6 @@
+// Entry point
+import Hello from './hello';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Render
+ReactDOM.render(<Hello />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: './app/app.js',
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loaders: ['react-hot','babel-loader']
+			}
+		]
+	}
+}
